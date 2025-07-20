9b0cb758d543a0f1d92ab08a479071e1e77b93af
Rajeev R Sharma <i.rarsh@gmail.com>
2025-07-20 21:56:28 +0530

    chore: remove tools page h1 and moe the credits to header

--- app/components/tools/Container.vue
+++ app/components/tools/Container.vue
@@ -1,17 +1,12 @@
 <template>
   <AppContainer :title :padding="false">
+    <template #actions>
+      <UBadge color="neutral" variant="subtle" size="lg">
+        Credits: <span class="font-bold">{{ user?.credits || 0 }}</span>
+      </UBadge>
+    </template>
     <UContainer class="py-4 sm:py-6 lg:py-8">
       <div class="flex flex-col gap-6 md:gap-8">
-        <div class="flex items-center justify-between gap-4 mb-8">
-          <h1 class="text-3xl font-bold tracking-tight text-highlighted">
-            {{ title }}
-          </h1>
-
-          <UBadge color="neutral" variant="subtle" size="lg">
-            Credits: <span class="font-bold">{{ credits }}</span>
-          </UBadge>
-        </div>
-
         <slot />
 
         <h3 class="text-2xl font-semibold ">How It Works</h3>
@@ -32,11 +27,12 @@
 <script setup lang="ts">
 defineProps<{
   title: string
-  credits: number
   howItWorks: {
     icon: string
     title: string
     description: string
   }[]
 }>()
+
+const { user } = useUserSession()
 </script>
